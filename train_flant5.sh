f446d944717b4a15adff7bd187e5588a7e08d9b1
pengfeiduan <pengfeiduan@tencent.com>
2023-06-08 21:13:53 +0800

    update flant5.sh

--- train_flant5.sh
+++ train_flant5.sh
@@ -4,7 +4,7 @@ torchrun --nproc_per_node=1 --master_port=9778 fastchat/train/train_flant5.py \
     --model_name_or_path /home/fangbing/minillm/flan-t5-base  \
     --data_path /home/fangbing/minillm/chatfine/total_filtered.json \
     --bf16 True \
-    --output_dir ./checkpoints_flant5_3b \
+    --output_dir ./checkpoints_flant5_base \
     --num_train_epochs 3 \
     --per_device_train_batch_size 1 \
     --per_device_eval_batch_size 1 \
@@ -22,5 +22,7 @@ torchrun --nproc_per_node=1 --master_port=9778 fastchat/train/train_flant5.py \
     --fsdp_transformer_layer_cls_to_wrap T5Block \
     --model_max_length 2048 \
     --preprocessed_path ./preprocessed_data/processed.json \
+    --lazy_preprocess False > run_fant5.out 2> run_fant5.err &
+
 #    --gradient_checkpointing True
     #    --tf32 True \
