cf627e58b6563790bc96eb9fdf643e48da1e695e
pengfeiduan <pengfeiduan@tencent.com>
2023-06-08 21:27:43 +0800

    update flant5.sh

--- train_flant5.sh
+++ train_flant5.sh
@@ -7,8 +7,8 @@ torchrun --nproc_per_node=1 --master_port=9778 fastchat/train/train_flant5.py \
     --bf16 True \
     --output_dir ./checkpoints_flant5_base \
     --num_train_epochs 3 \
-    --per_device_train_batch_size 1 \
-    --per_device_eval_batch_size 1 \
+    --per_device_train_batch_size 8 \
+    --per_device_eval_batch_size 8 \
     --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
@@ -21,7 +21,7 @@ torchrun --nproc_per_node=1 --master_port=9778 fastchat/train/train_flant5.py \
     --logging_steps 1 \
     --fsdp "full_shard auto_wrap" \
     --fsdp_transformer_layer_cls_to_wrap T5Block \
-    --model_max_length 2048 \
+    --model_max_length 2662 \
     --preprocessed_path ./preprocessed_data/processed.json \
     --lazy_preprocess False > run_fant5.out 2> run_fant5.err &
 
