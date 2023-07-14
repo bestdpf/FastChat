5a01b32d982c3d33108351335370edfebe116b53
pengfeiduan <pengfeiduan@tencent.com>
2023-07-15 02:12:32 +0800

    update run serv

--- run_serv.sh
+++ run_serv.sh
@@ -3,6 +3,6 @@ export PYHONPATH=.
 export DS_ACCELERATOR="cuda"
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:64
 python3 -m fastchat.serve.controller --host localhost --port 21001 >run_serv_controller.out 2> run_serv_controller.err &
-# CUDA_VISIBLE_DEVICES=0 python3 -m fastchat.serve.model_worker --port 21003 --worker-address 'http://localhost:21003' --model-name 'vicuna-wizard-7b' --model-path ./fastchat-vicuna-wizardbf16 --device cuda > run_serv_worker.out 2> run_serv_worker.err &
-CUDA_VISIBLE_DEVICES=7 python3 -m fastchat.serve.model_worker --port 21004 --worker-address 'http://localhost:21004' --model-name 'vicuna-wizard-7b' --model-path ./fastchat-vicuna-wizardbf16 --device cuda --load-8bit > run_serv_worker.out 2> run_serv_worker.err &
+CUDA_VISIBLE_DEVICES=0 python3 -m fastchat.serve.model_worker --port 21003 --worker-address 'http://localhost:21003' --model-name 'vicuna-wizard-7b' --model-path ./fastchat-vicuna-wizardbf160712 --device cuda > run_serv_worker.out 2> run_serv_worker.err &
+CUDA_VISIBLE_DEVICES=1 python3 -m fastchat.serve.model_worker --port 21004 --worker-address 'http://localhost:21004' --model-name 'vicuna-wizard-7b' --model-path ./fastchat-vicuna-wizardbf160712 --device cuda > run_serv_worker.out 2> run_serv_worker.err &
 python3 -m fastchat.serve.openai_api_server --host 0.0.0.0 --port 9999 > run_serv_api.out 2> run_serv_api.err&
