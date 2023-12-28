f5ea52fc995588ea8c9ee76104cef32242f88b88
pengfeiduan <pengfeiduan@tencent.com>
2023-12-28 13:59:20 +0800

    new upload.py

--- scripts/upload.py
+++ scripts/upload.py
@@ -2,11 +2,14 @@ from huggingface_hub import HfApi
 import sys
 
 
-def upload_model(model_dir, repo_id):
+def upload_model(model_dir, repo_id, repo_type):
     print(f'uploading {model_dir} to {repo_id}')
     api = HfApi()
-    api.upload_folder(folder_path=model_dir, repo_id=repo_id)
+    api.upload_folder(folder_path=model_dir, repo_id=repo_id, repo_type=repo_type)
 
 
 if __name__ == '__main__':
-    upload_model(sys.argv[1], sys.argv[2])
+    if len(sys.argv) != 4 or sys.argv[3] not in ['dataset', 'model']:
+        print(f'usage: python upload.py local_path remote_repo_id repo_type, repo_type must be "dataset" or "model"')
+        exit(-1)
+    upload_model(sys.argv[1], sys.argv[2], sys.argv[3])
