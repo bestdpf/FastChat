8db7fb5e14381ae86eafaf4b489ecef7ebcbcad5
pengfeiduan <pengfeiduan@tencent.com>
2024-04-21 12:48:00 +0800

    train_no deep

--- train_wizard.sh
+++ train_wizard.sh
@@ -1,8 +1,11 @@
 export OMP_NUM_THREADS=8
 export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:64
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=1,3,4,5
+export HF_DATASETS_IN_MEMORY_MAX_SIZE=20000000000
+export HF_DATASETS_OFFLINE=0
 export PYTHONPATH=.
-torchrun --nproc_per_node=8 --master_port=20011 fastchat/train/train_mem.py \
+
+python -m torch.distributed.run --nproc_per_node=4 --master_port=20011 fastchat/train/train_mem.py \
     --model_name_or_path ../Meta-Llama-3-8B  \
     --data_path ../chatfine/total_gpt_full_merge_b16_20240420 \
     --bf16 True \
@@ -14,7 +17,7 @@ torchrun --nproc_per_node=8 --master_port=20011 fastchat/train/train_mem.py \
     --gradient_accumulation_steps 16 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
-    --save_steps 500 \
+    --save_steps 300 \
     --save_total_limit 3 \
     --learning_rate 2e-5 \
     --weight_decay 0. \
@@ -23,7 +26,7 @@ torchrun --nproc_per_node=8 --master_port=20011 fastchat/train/train_mem.py \
     --logging_steps 1 \
     --fsdp "full_shard auto_wrap offload" \
     --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
-    --model_max_length 2048 \
+    --model_max_length 8192 \
     --gradient_checkpointing True \
     --lazy_preprocess True >run_wizard.out 2>run_wizard.err&
 
