7d6b7b333ea7ab40d63eda29c7b8560e667ab6f6
user <user@heyi.com>
2024-04-21 21:35:41 +0800

    svr

--- train_wizard.sh
+++ train_wizard.sh
@@ -7,16 +7,16 @@ export PYTHONPATH=.
 export NCCL_P2P_DISABLE="1"
 export NCCL_IB_DISABLE="1"
 
-python -m torch.distributed.run --nproc_per_node=4 --master_port=20011 fastchat/train/train_mem.py \
+python -m torch.distributed.run --nproc_per_node=1 --master_port=20011 fastchat/train/train_mem.py \
     --model_name_or_path ../Meta-Llama-3-8B  \
     --data_path ../chatfine/total_gpt_full_merge_b16_20240420 \
     --bf16 True \
     --tf32 True \
     --output_dir fastchat-vicuna-3-8b-20240420 \
     --num_train_epochs 3 \
-    --per_device_train_batch_size 4 \
-    --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 16 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 1 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 300 \
@@ -28,7 +28,7 @@ python -m torch.distributed.run --nproc_per_node=4 --master_port=20011 fastchat/
     --logging_steps 1 \
     --fsdp "full_shard auto_wrap offload" \
     --fsdp_transformer_layer_cls_to_wrap 'LlamaDecoderLayer' \
-    --model_max_length 8192 \
+    --model_max_length 2048 \
     --gradient_checkpointing True \
     --lazy_preprocess True >run_wizard.out 2>run_wizard.err&
 
