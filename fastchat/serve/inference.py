e33e67f4365f9dbbfd11b2261cf4a03ea5dfc6f3
pengfeiduan <pengfeiduan@tencent.com>
2024-08-19 22:19:00 +0800

    test bf16 do_sample=True v2

--- fastchat/serve/inference.py
+++ fastchat/serve/inference.py
@@ -278,7 +278,7 @@ def generate_stream_v2(
 
         output_ids = model.generate(input_ids=torch.as_tensor([input_ids], device=device),
                                     # max_length=max_new_tokens,
-                                    do_sample=False,
+                                    do_sample=True,
                                     max_new_tokens=max_new_tokens,
                                     temperature=temperature, repetition_penalty=repetition_penalty,
                                     use_cache=True, top_p=top_p, top_k=top_k)
