04527946a443e5682fdecb80fa8575e5e8c0db48
pengfeiduan <pengfeiduan@tencent.com>
2025-03-08 11:08:23 +0800

    test train 10000

---
+++ train_cot_nohup.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+
+export CUDA_VISIBLE_DEVICES=0
+export PYTHONPATH=.
+export OMP_NUM_THREADS=10
+export DS_ACCELERATOR="cuda"
+export PYTORCH_CUDA_ALLOC_CONF=max_split_size_mb:64
+#export WANDB_DISABLED="true"
+export HF_DATASETS_IN_MEMORY_MAX_SIZE=10000000000
+export HF_DATASETS_OFFLINE=0
+export TRANSFORMERS_OFFLINE=0
+export NCCL_P2P_DISABLE="1"
+export NCCL_IB_DISABLE="1"
+nohup python -m torch.distributed.run --nproc_per_node=1 --master_port=20012 fastchat/train/train_mem.py \
+    --model_name_or_path ./fastchat-vicuna-3-8b-20240906-full  \
+    --data_path ../deepevo/back_10000.json\
+    --bf16 True \
+    --tf32 True \
+    --output_dir fastchat-vicuna-3-8b-20250308 \
+    --num_train_epochs 1 \
+    --per_device_train_batch_size 1 \
+    --per_device_eval_batch_size 1 \
+    --gradient_accumulation_steps 2 \
+    --evaluation_strategy "steps" \
+    --save_strategy "steps" \
+    --save_steps 300 \
+    --eval_steps 300 \
+    --save_total_limit 3 \
+    --learning_rate 2e-5 \
+    --weight_decay 0. \
+    --warmup_ratio 0.03 \
+    --lr_scheduler_type "cosine" \
+    --logging_steps 1 \
+    --deepspeed "default_offload_opt_param.json" \
+    --report_to "wandb" \
+    --model_max_length 2048 \
+    --gradient_checkpointing True \
+    --lazy_preprocess False >run_wizard_nohup.out 2>run_wizard_nohup.err&
+tail -f run_wizard.*
+    #--deepspeed "./ds_flan_t5_z3_config_bf16.json" \
